a19f79895a63c33f0d7169c091fa9d95962b272e
Jnpacheco-125 <RM350127@fiap.com.br>
2023-06-12 21:36:31 -0300

    Primeiro commit2

---
+++ src/main/java/com/grupo38/automatizandoenergia/AutomatizandoEnergiaApplication.java
@@ -0,0 +1,13 @@
+package com.grupo38.automatizandoenergia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatizandoEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatizandoEnergiaApplication.class, args);
+	}
+
+}
